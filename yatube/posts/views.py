1bf51ca3ed13e8b8f16bba52028a050435e8bf33
Семен Новиков <novikreef@gmail.com>
2021-11-30 21:14:09 +0200

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request):
+    return HttpResponse('Страница с группами')
+
+
+def group_posts_detail(request, pk):
+    return HttpResponse(f'Пост номер {pk}')
+
 
 # Create your views here.
