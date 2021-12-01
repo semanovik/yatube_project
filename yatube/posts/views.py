91bffc29679badaf7a113adfe3d538da3602e798
Семен Новиков <novikreef@gmail.com>
2021-12-01 22:08:20 +0200

    Добавил главную страницу

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request):
