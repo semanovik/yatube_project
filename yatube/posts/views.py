46a194d4fd1f96aacec7a8566f2d4fbf1c82d0a8
Семен Новиков <novikreef@gmail.com>
2021-12-02 22:46:44 +0200

    Добавил лого и бутстрап

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,12 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    context = {
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
 
 
 def group_posts(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
 
 
 
